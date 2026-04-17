78e0ea4d958e807c6708be06c19ac7a11a0d858b
Andrew Ma <andrewwmaa@gmail.com>
2026-04-17 18:01:52 +0000

    consumer update

--- tech_lab_on_campus/market_watch/producer_and_consumer/consumer/solution/consumer_sol.py
+++ tech_lab_on_campus/market_watch/producer_and_consumer/consumer/solution/consumer_sol.py
@@ -10,33 +10,43 @@ class mqConsumer(mqConsumerInterface):
         self.binding_key = binding_key
         self.exchange_name = exchange_name
         self.queue_name = queue_name
+        self.connection = None
+        self.channel = None
+        self.setupRMQConnection()
 
     def setupRMQConnection(self):
         con_params = pika.URLParameters(os.environ["AMQP_URL"])
-        connection = pika.BlockingConnection(parameters=con_params)
-        channel = connection.channel()
-        exchange = channel.exchange_declare(exchange=self.exchange_name)
+        self.connection = pika.BlockingConnection(parameters=con_params)
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=self.exchange_name)
 
-        channel.queue_declare(queue=self.queue_name)
+        self.channel.queue_declare(queue=self.queue_name)
 
-        channel.queue_bind(
+        self.channel.queue_bind(
             queue=self.queue_name,
             routing_key=self.binding_key,
             exchange=self.exchange_name,
         )
 
-        channel.basic_consume(
-            self.queue_name, on_message_callback=self.callback, auto_ack=False
+        self.channel.basic_consume(
+            self.queue_name, on_message_callback=self.on_message_callback, auto_ack=False
         )
 
-        
 
+def on_message_callback(self, channel, method_frame, body):
         channel.basic_ack(method_frame.delivery_tag, False)
-
-
-        channel.start_consuming()
+        print(body.decode('utf-8'))
 
 
         channel.close()
-        connection.close()
+        self.connection.close()
 
+def startConsuming(self):
+    print(" [*] Waiting for messages. To exit press CTRL+C")
+    self.channel.start_consuming()
+
+def __del__(self):
+     print("Closing RMQ connection on destruction")
+     self.channel.close()
+
+     self.connection.close()
