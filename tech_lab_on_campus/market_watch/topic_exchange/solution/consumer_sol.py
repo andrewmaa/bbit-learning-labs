b3992f59ac865f819e36d9941fc722bf5fbbe36e
Andrew Ma <andrewwmaa@gmail.com>
2026-04-17 19:45:22 +0000

    Refactor mqConsumer to support multiple binding keys for topic exchange

--- tech_lab_on_campus/market_watch/topic_exchange/solution/consumer_sol.py
+++ tech_lab_on_campus/market_watch/topic_exchange/solution/consumer_sol.py
@@ -1,11 +1,11 @@
 import pika
 import os
 from consumer_interface import mqConsumerInterface
-
+from collections import defaultdict
 
 class mqConsumer(mqConsumerInterface):
-    def __init__(self, binding_key, exchange_name, queue_name):
-        self.binding_key = binding_key
+    def __init__(self, binding_keys, exchange_name, queue_name):
+        self.binding_keys = binding_keys if isinstance(binding_keys, list) else [binding_keys]
         self.exchange_name = exchange_name
         self.queue_name = queue_name
         self.connection = None
@@ -20,17 +20,22 @@ class mqConsumer(mqConsumerInterface):
 
         self.channel.queue_declare(queue=self.queue_name)
 
-        self.channel.queue_bind(
-            queue=self.queue_name,
-            routing_key=self.binding_key,
-            exchange=self.exchange_name,
-        )
+        self.bindQueueToExchange(self.queue_name, self.exchange_name)
 
         self.channel.basic_consume(
             self.queue_name, on_message_callback=self.on_message_callback, auto_ack=False
         )
 
+    def bindQueueToExchange(self, queueName: str, topic: str):
+        for binding_key in self.binding_keys:
+            self.channel.queue_bind(
+                        queue=queueName,
+                        routing_key=binding_key,
+                        exchange=topic,
+                    )
 
+
+        
     def on_message_callback(self, channel, method_frame, header_frame, body):
             channel.basic_ack(method_frame.delivery_tag, False)
             print(body.decode('utf-8'))
